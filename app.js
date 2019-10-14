b98fa896127d6d9e337694c714a3a9500ba6aeff
ratistiss <50683985+ratistiss@users.noreply.github.com>
2019-10-14 13:23:46 -0400

    Coding 1.2
    Erased and started again using fetch() was able to get gallery up and running

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
-
-$.ajax({
-    url: 'https://randomuser.me/api/?results=12&&inc=picture,name,email,location,phone,dob',
-    dataType: 'json',
-    success: function (data) {
-        console.log(data);
-        generateCards(data);
-    }
-});
-
-
+const gallery = $('#gallery');
+const search = $('.search-container');
+fetch('https://randomuser.me/api/?results=12&&nat=us&&inc=picture,name,email,location,phone,dob')
+    .then(response => response.json())
+    .then(response => generateCards(response))
 // Search Markup 
 $('.search-container').append('<form></form>');
 const form = $('form').attr("action", "#").attr("method", "get");
 form.append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
 form.append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
-
-
-
 //Gallery Markup
-let card = $('#gallery').append('<div class="card">');
-let imgCont = $('.card').append('<div class="card-img-container">');
-let infoCont = $('.card').append('<div class="card-info-container"></div>');
-let img = $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></img>');
-let name = $('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
-let email = $('.card-info-container').append('<p class="card-text">email</p>');
-let place = $('.card-info-container').append('<p class="card-text cap">city, state</p>');
+function generateCards(response) {
+    for (let i = 0; i < response.results.length; i++) {
+        const picture = response.results[i].picture.thumbnail;
+        const name = response.results[i].name.first + ', ' + response.results[i].name.last;
+        const email = response.results[i].email;
+        const location = response.results[i].location.city + ', ' + response.results[i].location.state;
+        const galleryHTML = `<div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src='${picture}' alt="profile picture">
+                    </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${name}</h3>
+                    <p class="card-text">${email}</p>
+                    <p class="card-text cap">${location}</p>
+                </div>
+            </div>
+         `
+        gallery.append(galleryHTML);
+    }
+};
 
-    function generateCards(people) {
-            console.log(people.dob);
-
-   }
-
+ 
 //Modal markup
-
-    
+function generateModal(response){
+    for (let i = 0; i < response.results.length; i++) {
+        const picture = response.results[i].picture.medium;
+        const name = response.results[i].name.first + ', ' + response.results[i].name.last;
+        const email = response.results[i].email;
+        const city = response.results[i].location.city + ', ' + response.results[i].location.state;
+        const phone = response.results[i].phone;
+        const address = response.results[i].location;
+        const birthday = response.results[i].dob.date.slice(0,10);
+        const modalHTML = `<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${picture}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${name}</h3>
+                <p class="modal-text">${email}</p>
+                <p class="modal-text cap">${city}</p>
+                <hr>
+                    <p class="modal-text">${phone}</p>
+                    <p class="modal-text">${location}</p>
+                    <p class="modal-text">Birthday: ${birthday}</p>
+                    </div>
+                </div>`
+                console.log(birthday);
+    }}
